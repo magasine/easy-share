881b9d8eaad293202ff313193f9d5e4568e8ec11
Manoel Garcia da Silveira Neto <magasine@gmail.com>
2025-07-12 19:19:30 -0300

    v20250709 - Update EasyShare.js text button

--- EasyShare.js
+++ EasyShare.js
@@ -1299,7 +1299,7 @@ javascript: (() => {
   Highlights List
 </button>
 <button id="tab-citation" class="tab-btn active" role="tab" aria-selected="false" aria-controls="citation-tab">
-  Citation Compose
+  Load Citation
 </button>
 </nav>
 
